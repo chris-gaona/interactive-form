2595deac2e988c2d07e73e9bbfe781edab21b69c
Chris Gaona <thebeloved88@sbcglobal.net>
2016-05-04 15:07:52 -0700

    Hides color t-short options initially until theme is picked

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function() {
   'use strict';
 
-  //Set focus on the first text field
+  //Set focus on the first text field on page load
   $('input#name').focus();
 
   //Job Role section of the form. Reveal a text field when the "Other" option is selected from the "Job Role" drop down menu
@@ -19,6 +19,9 @@ $(function() {
   });
 
   //T-Shirt Info section of the form. For the T-Shirt color menu, only display the options that match the design selected in the "Design" menu.
+
+  //Extra Credit: Hide the "Color" label and select menu until a T-Shirt design is selected from the "Design" menu.
+  $('#colors-js-puns').hide();
   var optionsArray = $('#color option').toArray();
 
   var intialOption = '<option><-- Please select a T-shirt theme</option>';
@@ -28,21 +31,22 @@ $(function() {
     var designOption = $(this).val();
 
     if (designOption === 'js puns') {
+      $('#colors-js-puns').show();
       $('select#color').empty().append(optionsArray[0]).append(optionsArray[1]).append(optionsArray[2]);
 
     } else if (designOption === 'heart js') {
+      $('#colors-js-puns').show();
       $('select#color').empty().append(optionsArray[3]).append(optionsArray[4]).append(optionsArray[5]);
 
     } else {
       $('select#color').empty().append(intialOption);
+      $('#colors-js-puns').hide();
     }
   });
 
   //Register for Activities section of the form.
 
   //Some events are at the same time as others. If the user selects a workshop, don't allow selection of a workshop at the same date and time -- you should disable the checkbox and visually indicate that the workshop in the competing time slot isn't available.
-  var labelsArray = $('fieldset.activities label').toArray();
-
   $('fieldset.activities input').on('change', function() {
     var text = $(this).closest('label').text();
     //splits the string at — & use pop to return the last
@@ -86,7 +90,7 @@ $(function() {
     if ($(this).is(':checked') && $('p#total-dollars').text().length === 0) {
       $('fieldset.activities').after(amount);
 
-    } else if ($(this).is(':checked') && $('p#total-dollars').text().length != 0){
+    } else if ($(this).is(':checked') && $('p#total-dollars').text().length !== 0){
       initialAmount = $('p#total-dollars').text().split('$');
       newAmount = parseInt(initialAmount[1]) + parseInt(money[1]);
       $('p#total-dollars').text('Total: $' + newAmount);
@@ -117,7 +121,7 @@ $(function() {
     var paymentOption = $(this).val();
 
     if (paymentOption === 'credit card') {
-      $('#credit-card').show().next('div').hide().next('div').hide();;
+      $('#credit-card').show().next('div').hide().next('div').hide();
 
     } else if (paymentOption === 'paypal') {
       $('#credit-card').hide().next('div').show().next('div').hide();
@@ -219,5 +223,11 @@ $(function() {
     }
 
   });
+
+
+  //Style the "select" menus (drop down menus) on the form, so they match the styling of the text fields (see Resources links for an article on how to improve the look of select menus using CSS and JavaScript).
+
+
+  //Validate the credit card number so that it's a validly formatted credit card number. (see the Resources links for information on how to do this.)
 
 });
