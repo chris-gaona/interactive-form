a344387f703850bb2c3cea6d22e782e35765cf3a
Chris Gaona <thebeloved88@sbcglobal.net>
2016-05-03 20:20:58 -0700

    Displays options that match design selected

--- js/main.js
+++ js/main.js
@@ -8,13 +8,30 @@ $(function() {
   $('select#title').on('change', function() {
     var otherOption = $(this).val();
 
-    if (otherOption == 'other') {
-      console.log('Yes!');
+    if (otherOption === 'other') {
       $('fieldset#main').append(otherInput);
 
     } else {
-      console.log('No!');
       $('fieldset#main').find('#other-title').remove();
+    }
+  });
+
+  var optionsArray = $('#color option').toArray();
+
+  var intialOption = '<option><-- Please select a T-shirt theme</option>';
+  $('select#color').empty().append(intialOption);
+
+  $('select#design').on('change', function() {
+    var designOption = $(this).val();
+
+    if (designOption === 'js puns') {
+      $('select#color').empty().append(optionsArray[0]).append(optionsArray[1]).append(optionsArray[2]);
+
+    } else if (designOption === 'heart js') {
+      $('select#color').empty().append(optionsArray[3]).append(optionsArray[4]).append(optionsArray[5]);
+
+    } else {
+      $('select#color').empty().append(intialOption);
     }
   });
 
