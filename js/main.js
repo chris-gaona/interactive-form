6f9b44166b7378591b7cf0bc39ee7c01b14d6a6e
Chris Gaona <thebeloved88@sbcglobal.net>
2016-05-03 19:15:07 -0700

    Displays extra input when other is selected

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 $(function() {
   'use strict';
 
-  $('fieldset input#name').focus();
+  $('input#name').focus();
+
+  var otherInput = '<input type="text" id="other-title" placeholder="Your Title">';
+
+  $('select#title').on('change', function() {
+    var otherOption = $(this).val();
+
+    if (otherOption == 'other') {
+      console.log('Yes!');
+      $('fieldset#main').append(otherInput);
+
+    } else {
+      console.log('No!');
+      $('fieldset#main').find('#other-title').remove();
+    }
+  });
+
 });
